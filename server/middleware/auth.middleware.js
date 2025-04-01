c6d9afd2398fdb40054c37408fe8cc188118494e
KONATHALA YASWANTH <132152566+yash-io@users.noreply.github.com>
2025-04-01 17:16:09 +0530

    Update auth.middleware.js

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -90,7 +90,7 @@ const LogoutAuth = (req, res) => {
   res.cookie("token", "", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "Strict",
+    sameSite: "None",
     expires: new Date(0), // Expire immediately
   });
   res.status(200).json({ success: true, message: "Logged out successfully" });
