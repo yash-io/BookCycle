07ae0f1c3eccdddc1e0efd8edc1ca1303581c95f
KONATHALA YASWANTH <132152566+yash-io@users.noreply.github.com>
2025-04-06 20:30:11 +0530

    Update filterPage.jsx

--- client/src/pages/filterPage.jsx
+++ client/src/pages/filterPage.jsx
@@ -57,7 +57,7 @@ const FilterPage = () => {
 
   return (
     <div className="w-full bg-gray-900 text-white rounded-sm p-4 shadow-lg border border-gray-700 dark:bg-gray-800 md:min-h-screen">
-      <div className="flex flex-col">
+     <div className="flex flex-col items-center md:items-start text-center md:text-left">
         {/* Toggle Filter Button */}
         <h2
           className="text-xl font-semibold mb-4 text-white border-2 border-white rounded-sm px-4 py-2 cursor-pointer w-32 md:w-full text-center"
