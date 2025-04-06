cf28b84b1027d15eb69a4a4a5cbfc3aa59d41947
KONATHALA YASWANTH <132152566+yash-io@users.noreply.github.com>
2025-04-06 20:47:17 +0530

    Update freeMaterial.jsx

--- client/src/pages/freeMaterial.jsx
+++ client/src/pages/freeMaterial.jsx
@@ -6,8 +6,7 @@ import Loading from "../components/isLoading";
 
 const MaterialHub = () => {
   const { materials, fetchMaterials, filteredMaterials } = UseMaterials();
-  const [isLoading, setIsLoading] = useState(false);
-  const [showFilter, setShowFilter] = useState(false); // Toggle for mobile
+  const [isLoading, setIsLoading] = useState(false); // Toggle for mobile
 
   useEffect(() => {
     fetchMaterials();
@@ -19,18 +18,11 @@ const MaterialHub = () => {
 
   return (
     <div className="container m-auto min-h-screen p-6 flex flex-col md:flex-row relative">
-      {/* Mobile Filter Button */}
-      <button
-        onClick={() => setShowFilter(!showFilter)}
-        className="md:hidden absolute top-20 left-4 bg-blue-600 text-white px-4 py-2 rounded shadow-lg z-50"
-      >
-        {showFilter ? "Close Filter" : "Open Filter"}
-      </button>
+      
 
       {/* Sidebar Filter - Normal on Desktop, Overlay on Mobile */}
       <div
   className={`fixed top-0 left-0 w-3/4 sm:w-2/4 h-full bg-gray-900 text-white shadow-lg p-4 transition-transform transform z-40
-    ${showFilter ? "translate-x-0" : "-translate-x-full"}
     md:translate-x-0 md:relative md:w-1/4 lg:w-1/5 md:mr-6 md:h-auto md:p-0 md:z-0`}
 >
   <FilterPage />
