5ff1267d87b84f8f90236aca948c91c4479bff73
KONATHALA YASWANTH <132152566+yash-io@users.noreply.github.com>
2025-04-06 21:05:56 +0530

    Update freeMaterial.jsx

--- client/src/pages/freeMaterial.jsx
+++ client/src/pages/freeMaterial.jsx
@@ -6,7 +6,7 @@ import Loading from "../components/isLoading";
 
 const MaterialHub = () => {
   const { materials, fetchMaterials, filteredMaterials } = UseMaterials();
-  const [isLoading, setIsLoading] = useState(false); // Toggle for mobile
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     fetchMaterials();
@@ -17,19 +17,13 @@ const MaterialHub = () => {
   }, [materials]);
 
   return (
-    <div className="container m-auto min-h-screen p-6 flex flex-col md:flex-row relative">
-      
+    <div className="container m-auto min-h-screen p-6 flex flex-col md:flex-row">
+      {/* Sidebar filter (responsive) */}
+      <div className="w-full md:w-1/4 lg:w-1/5 md:mr-6 mb-6 md:mb-0">
+        <FilterPage />
+      </div>
 
-      {/* Sidebar Filter - Normal on Desktop, Overlay on Mobile */}
-      <div
-  className={`fixed top-0 left-0 w-3/4 sm:w-2/4 h-full bg-gray-900 text-white shadow-lg p-4 transition-transform transform z-40
-    md:translate-x-0 md:relative md:w-1/4 lg:w-1/5 md:mr-6 md:h-auto md:p-0 md:z-0`}
->
-  <FilterPage />
-  </div>
-
-
-      {/* Main Content */}
+      {/* Main content */}
       <div className="flex-1">
         {!isLoading ? (
           <>
