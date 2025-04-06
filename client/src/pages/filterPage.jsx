f8837a13b03d58d03b0ca898366c0583e5a47c77
KONATHALA YASWANTH <132152566+yash-io@users.noreply.github.com>
2025-04-06 21:49:39 +0530

    Update filterPage.jsx

--- client/src/pages/filterPage.jsx
+++ client/src/pages/filterPage.jsx
@@ -56,7 +56,7 @@ const FilterPage = () => {
   }, [filters, materials, setFilteredMaterials]);
 
   return (
-    <div className="fixed left-0 top-16 w-full md:w-1/5 bg-gray-900 text-white rounded-sm p-2 md:p-4 shadow-lg border border-gray-700 dark:bg-gray-800">
+    <div className=" left-0 top-16 w-full md:w-1/5 bg-gray-900 text-white rounded-sm p-2 md:p-4 shadow-lg border border-gray-700 dark:bg-gray-800">
       <div className="flex flex-col items-center">
         <h2
           className="text-xl font-semibold mb-4 text-white border-2 border-white rounded-sm px-4 py-2 cursor-pointer w-32 md:w-auto text-center"
